64b35583c37d572ae5f522ac102f594a6b963fc5
Ekam jeet singh Bhullar <ekamjeetsinghbhullar412@gmail.com>
2026-09-06 08:12:38 +0530

    Add microphone icon import to MainScreen.ktm

--- app/src/main/java/com/jarvis/assistant/ui/screens/MainScreen.kt
+++ app/src/main/java/com/jarvis/assistant/ui/screens/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
